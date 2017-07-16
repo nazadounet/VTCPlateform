8bbd04266c71cd9ca687d7dfde20b3130a668fe7
Sissi <sihame.wi@gmail.com>
2017-07-16 23:07:42 +0200

    new modif
    Dimanche 16 juillet : 23h07

--- www/js/app.js
+++ www/js/app.js
@@ -83,5 +83,5 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/inscription');
+  $urlRouterProvider.otherwise('/historique');
 });
